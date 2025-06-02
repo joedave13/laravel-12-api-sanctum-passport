8b88e2ca0bbc511bd87c41394163ee28c1c65b7a
Joshua Davian <joshuadavian@gmail.com>
2025-06-02 14:33:33 +0700

    Register API

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\RegisterRequest;
+use App\Http\Resources\AuthResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = Hash::make($request->password);
+
+        $user = User::query()->create($data);
+
+        $accessToken = $user->createToken('auth_token')->plainTextToken;
+
+        return new AuthResource(['user' => $user, 'access_token' => $accessToken]);
+    }
 }
