d092566c2a988cce484b25a119f3c468832a39fa
Joshua Davian <joshuadavian@gmail.com>
2025-06-02 14:31:42 +0700

    API resource

---
+++ app/Http/Resources/AuthResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'user' => new UserResource($this['user']),
+            'access_token' => $this['access_token'],
+            'token_type' => 'Bearer'
+        ];
+    }
+}
