4e854eba24fb4f6b4e8a4c0d6984e32f8c3d3053
Joshua Davian <joshuadavian@gmail.com>
2025-06-02 11:54:51 +0700

    Login API

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\LoginRequest;
 use App\Http\Requests\Api\RegisterRequest;
 use App\Http\Resources\AuthResource;
 use App\Models\User;
@@ -17,6 +18,21 @@ class AuthController extends Controller
         $data['password'] = Hash::make($request->password);
 
         $user = User::query()->create($data);
+
+        $accessToken = $user->createToken('auth_token')->plainTextToken;
+
+        return new AuthResource(['user' => $user, 'access_token' => $accessToken]);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+
+        $user = User::query()->where('email', $data['email'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response()->json(['message' => 'Unauthenticated'], 401);
+        }
 
         $accessToken = $user->createToken('auth_token')->plainTextToken;
 
