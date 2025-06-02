d967fe2a2963bc54dd956c288893a5d48f0675ab
Joshua Davian <joshuadavian@gmail.com>
2025-06-02 14:44:16 +0700

    Fixing API routes

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:api')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
